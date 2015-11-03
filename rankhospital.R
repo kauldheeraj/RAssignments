8f3f7d9ab0a20862ea86998522e58d7efeb5890a
Dheeraj Kaul <kauldheeraj@gmail.com>
2015-11-02 20:32:47 -0500

    Committing Assignments for Coursera Assignment 3

--- rankhospital.R
+++ rankhospital.R
@@ -1,5 +1,5 @@
-rankhospital <- function (state, outcome, num){
-  data <- read.csv("outcome-of-care-measures.csv", colClasses="character")
+rankhospital <- function (state, outcome, num="best"){
+  data <- read.csv("/home/dheeraj/Desktop/Coursera/outcome-of-care-measures.csv", colClasses="character")
   strValidOutcome = c("heart failure","heart attack","pneumonia")
   
   if(nrow(data[data$State==state,]) == 0)
@@ -11,22 +11,22 @@ rankhospital <- function (state, outcome, num){
   outIndex <- NULL
   
   if (outcome =="heart attack")
-    ##subSetOfData <- data[data$State==state,][,11]
     outIndex <- 11
   else if (outcome =="heart failure")  
-    #subSetOfData <- data[data$State==state,][,17]
-    outIndex <- 17
+    outIndex <-17
   else if (outcome == "pneumonia") 
-    #subSetOfData <- data[data$State==state,][,23]
     outIndex <- 23
   
-  minVal <- min(as.numeric(subsetState[,outIndex]), na.rm = TRUE)
-  minAll <- subsetState[as.numeric(subsetState[,outIndex], na.rm = TRUE) == minVal, ]
-  finalSet <- minAll[!is.na(minAll[,2]),][,2]
-  print (finalSet)
-  ##ordInd <- order(minAll[,2], na.rm=TRUE)
-  ##print (ordInd)
-  #minAll[ordInd[1],][2]
-  #hosp_name <- state_subset[min_index, 2]
-  #return(hosp_name)
+  subsetState <- subsetState[order(subsetState[,outIndex], subsetState[,2], na.last = NA),]
+  subsetState <- subsetState[!is.na(as.numeric(subsetState[,outIndex])),]
+  ##print (subsetState[,2])
+  ##print (subsetState[,outIndex])
+  if (num == "best")
+    subsetState[1,2]
+  else if (num == "worst")
+    subsetState[nrow(subsetState),2]
+  else 
+    subsetState[num,2]
+  
+
 }
